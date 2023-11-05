f00731851403d166f18aee2199f82407d46f00db
andrek4z <andre.fernandez.98@hotmail.com>
2023-11-04 20:04:33 -0500

    Done

--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -39,7 +39,21 @@ public static class ArraysTester {
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return new double[0]; // replace this return statement with your own
+         // Create an empty list to store the multiples.
+        List<double> multiples = new List<double>();
+
+        // Set up a loop that iterates from 1 to the number of multiples.
+        for (int i = 1; i <= length; i++) 
+        {
+            // Calculate the current multiple by multiplying the starting number by the loop counter.
+            double currentMultiple = number * i;
+
+            // Add the current multiple to the list.
+            multiples.Add(currentMultiple);
+        }
+
+        // Return the list of multiples.
+        return multiples.ToArray(); // replace this return statement with your own
     }
     
     /// <summary>
@@ -56,6 +70,19 @@ public static class ArraysTester {
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        
+        // Create a temporary list to store the values to be moved to the end of the data list.
+        List<int> temp = new List<int>();
+
+        // Remove the specified number of elements from the end of the data list and add them to the temporary list.
+        for (int i = 0; i < amount; i++) 
+        {
+            temp.Add(data[data.Count - 1]);
+            data.RemoveAt(data.Count - 1);
+        }
+
+        // Insert the temporary list into the beginning of the data list.
+        data.InsertRange(0, temp);
 
     }
 }
