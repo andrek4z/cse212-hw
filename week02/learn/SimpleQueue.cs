aaa07def05c441dfbb8a24fdb741bd0d8970b028
andrek4z <andre.fernandez.98@hotmail.com>
2023-11-12 22:51:08 -0500

    Done

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -69,7 +69,7 @@ public class SimpleQueue
     /// <param name="value">Integer value to add to the queue</param>
     private void Enqueue(int value) 
     {
-        _queue.Insert(0, value);
+        _queue.Add(value);
     }
 
     /// <summary>
@@ -82,8 +82,8 @@ public class SimpleQueue
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var value = _queue[0];
+        _queue.RemoveAt(0);
         return value;
     }
 }
