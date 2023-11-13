f46a0684e954013e2b45c23bc70497345d38be40
andrek4z <andre.fernandez.98@hotmail.com>
2023-11-13 02:21:30 -0500

    Done

--- week02/code/Priority.cs
+++ week02/code/Priority.cs
@@ -23,6 +23,22 @@ public static class Priority
         // Scenario: 
         // Expected Result: 
         Console.WriteLine("Test 1");
+        priorityQueue = new PriorityQueue();
+
+        priorityQueue.Enqueue("High Priority Item", 1);
+        priorityQueue.Enqueue("Medium Priority Item", 2);
+        priorityQueue.Enqueue("Low Priority Item", 3);
+
+        //Console.WriteLine(priorityQueue);
+
+        var highPriorityItem = priorityQueue.Dequeue();
+        Console.WriteLine(highPriorityItem);
+
+        var mediumPriorityItem = priorityQueue.Dequeue();
+        Console.WriteLine(mediumPriorityItem);
+
+        var lowPriorityItem = priorityQueue.Dequeue();
+        Console.WriteLine(lowPriorityItem);
 
         // Defect(s) Found: 
 
@@ -32,6 +48,22 @@ public static class Priority
         // Scenario: 
         // Expected Result: 
         Console.WriteLine("Test 2");
+        priorityQueue = new PriorityQueue();
+
+        priorityQueue.Enqueue("High Priority Item 1", 1);
+        priorityQueue.Enqueue("High Priority Item 2", 1);
+        priorityQueue.Enqueue("Medium Priority Item", 2);
+
+        //Console.WriteLine(priorityQueue);
+
+        var highPriorityItem1 = priorityQueue.Dequeue();
+        Console.WriteLine(highPriorityItem1);
+
+        var highPriorityItem2 = priorityQueue.Dequeue();
+        Console.WriteLine(highPriorityItem2);
+
+        var mediumPriorityItem2 = priorityQueue.Dequeue();
+        Console.WriteLine(mediumPriorityItem2);
 
         // Defect(s) Found: 
 
